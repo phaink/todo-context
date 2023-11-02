56e26455fe7a30ffc9ed69974fcfaec85f99a40b
phaink <140713886+phaink@users.noreply.github.com>
2023-11-02 13:51:47 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-gray-800 h-screen w-full text-gray-300">
+      <h1 className="text-center py-4 text-4xl">ToDo List</h1>
       <AddTodo />
     </div>
   );
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,42 +1,46 @@
 import type { ITodo, TodoContextType } from "../@types/todo";
 
 export default function AddTodo() {
+  const formData = {}
+  const disable = formData === undefined ? true : false
+  console.log("disable: ", disable);
+
   return (
-    <form>
-      <div className="p-2 flex flex-col gap-4">
+    <form className="flex flex-col w-2/3 ">
+      <fieldset className="p-2 flex flex-col gap-6">
         <div className="flex gap-2 items-center">
           <label
-            className="block text-sm font-medium leading-6 text-gray-900 capitalize"
+            className="block text-sm font-medium leading-6 text-gray-100 capitalize"
             htmlFor="title"
           >
             title
           </label>
           <input
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-100 bg-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
             type="text"
             id="title"
           />
         </div>
         <div className="flex gap-2  items-center">
           <label
-            className="block text-sm font-medium leading-6 text-gray-900 capitalize"
+            className="block text-sm font-medium leading-6 text-gray-100 capitalize"
             htmlFor="description"
           >
             description
           </label>
           <input
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-100 bg-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
             type="text"
             id="description"
           />
         </div>
-      </div>
+      </fieldset>
       <button
         type="submit"
-        disabled={true}
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        disabled={disable}
+        className="flex my-4 mx-2 w-1/3 self-end justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600 capitalize"
       >
-        Save
+        save
       </button>
     </form>
   );
